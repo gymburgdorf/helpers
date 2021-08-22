6c79a5e7ba5531fccecb0ce989a578001967c351
adilu <adrian.luethi@gmail.com>
2021-08-23 01:03:30 +0200

    add script enhancer

---
+++ html_tags.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", enhance)
+
+function enhance() {
+	sections = document.querySelectorAll("section")
+	for(let s of sections) {
+		heading = s.querySelector("h2").outerHTML
+		code = s.innerHTML.slice(s.innerHTML.indexOf("</h2>") + 5)
+		s.innerHTML = `
+			<details>
+				<summary>${heading}</summary>
+					${code}
+				<h3>Code:</h3>
+				<code></code>
+			</details>
+		`
+		s.querySelector("code").textContent = code
+	}
+}
