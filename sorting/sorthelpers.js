2f8b5c7791ba44bc5dbbde612e8d17cf30b3e4b9
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-03-04 23:04:21 +0100

    add sorthelpers

---
+++ sorting/sorthelpers.js
@@ -0,0 +1,26 @@
+export function createNumberList(n) {
+    let list = []
+    for(let i = 0; i < n; i++) {
+        list.push(getRandomInt(0, 1000000))
+    }
+	return list
+}
+
+export function createObjectList(n) {
+    let list = []
+    for(let i = 0; i < n; i++) {
+        list.push({name: getRandomName, age: getRandomInt(1, 99)})
+    }
+	return list
+}
+
+function getRandomInt(low, high) {
+    return low + Math.floor(Math.random() * (high - low + 1));
+}
+
+function getRandomName() {
+    const vowels = "aeiou"
+    const consonants = "bcdfghjklmnprstvwxz"
+    const pick = (chars) => chars[getRandomInt(0, chars.length-1)]
+    return pick(consonants).toUpperCase() + pick(vowels) + pick(consonants) + pick(vowels)
+}
