5c4e14fda88b113af63f5a91e4cc8af4ba11e118
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-03-04 23:54:55 +0100

    renderList

--- sorting/sorthelpers.js
+++ sorting/sorthelpers.js
@@ -14,6 +14,16 @@ export function createObjectList(n) {
 	return list
 }
 
+function renderList(list) {
+    let element = document.querySelector(".output")
+    if(!element) {
+        document.body.insertAdjacentHTML("beforeend", `<div class="output"></div>`)
+        document.querySelector(".output")
+    }
+    element.innerHTML = list.slice(0,50).map(x=>`<span> ${JSON.stringify(x)} </span>`)
+    `
+}
+
 function getRandomInt(low, high) {
     return low + Math.floor(Math.random() * (high - low + 1));
 }
